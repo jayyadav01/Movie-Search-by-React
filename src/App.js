6e0a3b0763a8c3500f8fd7aa961cf533539bdb5e
jayyadav01 <jayyadav.rbt@gmail.com>
2023-08-01 01:02:02 +0530

    Movie Search

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
  const [ movieName , setmovieName] = useState([]);
  const [ data , setdata] = useState([]);
 
- function open()
+ function search()
  {
     axios.get("https://api.themoviedb.org/3/search/movie?api_key=6823d286f080c1f6a885aaaf7abb5e93&language=en-US&query=" +movieName+"&page=1&include_adult=false")
         .then((response) => {
@@ -18,6 +18,20 @@ function App() {
     })
 }
     let link = 'https://image.tmdb.org/t/p/original';
+
+    function trimTitle(tile)
+    {
+      return(
+        (tile.length > 15) ? tile.slice(0,15) + '...' : tile
+      )
+    }
+    function trimOverview(overview)
+    {
+      return(
+        (overview.length > 100) ? overview.slice(0,100) + '...' : overview
+      )
+    }
+    
   return (
     <>
       <div className='head'>
@@ -26,28 +40,35 @@ function App() {
         </div>
         <div className='search'>
           <input placeholder='Find movies, TV shows and movies' onChange={(e) => setmovieName(e.target.value)} />
-          <button onClick={open}>Search</button>
+          <button onClick={search}>Search</button>
         </div>
       </div>
 
       <div className='parent'>
         <div className='detail'>
         {
-          
-            data ?
-            data.map((data,i) => {
+              data.map((data,i) => {
                 return (
                     <div className='box' key={i}>
                       <div className='image'>
-                        <img id='movie' src={link+data.poster_path} width={235} height={338}></img>
+                        
+                      {
+                          (data.poster_path) ? 
+                            <img id='movie' src={link+data.poster_path} width={200} height={238} alt='poster'></img>
+                          :
+                            <img src='../Dummy.jpeg' width={200} height={238} alt='poster'></img>
+
+                      }
+
                       </div>
 
                       <div className='des'>
-                        <h1>{data.title}</h1>
+                        <h1>{trimTitle(data.title)}</h1>
+                        <p>{trimOverview(data.overview)}</p>
                       </div>
                     </div>
                 )
-            }) : ''
+            })
           
         }
       </div>
